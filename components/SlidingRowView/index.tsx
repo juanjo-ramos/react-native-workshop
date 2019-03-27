deed2d81ea6dc48f856bbb60f8be35f1045ae700
Juanjo Ramos <jjramos.developer@gmail.com>
2019-03-27 11:09:51 +0100

    Removing animations for exercise one

--- components/SlidingRowView/index.tsx
+++ components/SlidingRowView/index.tsx
@@ -7,37 +7,31 @@ interface Props extends RowViewProps {
   style?: StyleProp<ViewStyle>;
 }
 
-const initialPosition = -200;
-
-export default class SlidingRowView extends React.PureComponent<Props> {  
-  position = new Animated.Value(initialPosition);
-  opacity = new Animated.Value(0.5);
+export default class SlidingRowView extends React.PureComponent<Props> {
+  // TODO: Think what values makes sense for your animation
+  position = new Animated.Value(0);
+  opacity = new Animated.Value(1.0);
 
   componentDidMount() {
     this.animateAddOperation();
   }
 
   animateAddOperation() {
-    Animated.parallel([
-      Animated.timing(this.position, {
-        toValue: 0,
-        duration: 50,
-        easing: Easing.ease
-      }),
-      Animated.timing(this.opacity, {
-        toValue: 1,
-        duration: 50,
-        easing: Easing.ease
-      })
-    ]).start();
+    // TODO: Create the animation
+    // We need 2 animations to run in parallel
+    // 1. Translates the X component of the position to 0
+    // 2. Transforms the opacity of the Animated.View from 0.5 to 1.0
+    // They both should last 50ms and should use one `easing` option
+    // Extra kudos if you use `interpolate` for the opacity one    
   }
 
   animateRemoveOperation(onEnd?: () => void) {
-    Animated.timing(this.position, {
-      toValue: initialPosition,
-      duration: 75,
-      easing: Easing.ease
-    }).start(onEnd);
+    // TODO: Create the animation
+    // 1. We need to run a single animation that moves the position of the row
+    // outside the screen.
+    // 2. The animations lasts 75ms.
+    // 3. onEnd callback is called at the end of the animation
+    onEnd && onEnd();
   }
 
   render() {
