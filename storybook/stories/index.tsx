6ac28a3215b4c3f7a226296d7f42aed781ad546c
Juanjo Ramos <jjramos.developer@gmail.com>
2019-03-29 09:05:09 +0100

    Configure stories as in master

--- storybook/stories/index.tsx
+++ storybook/stories/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Dimensions, Text } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import TopToBottomRowAnimation from "./TopToBottomRowAnimation";
+import LeftToRightRowAnimation from "./LeftToRightRowAnimation";
 import LoginScreen from "./Login/LoginScreen";
 import ShakyText from "./ShakyText";
 import AnimatedBar from "./AnimatedBar";
@@ -9,40 +10,44 @@ import { seq } from "../../components/utils";
 import GrowAndDisappear from "./GrowAndDisappear";
 import Banner from "./Banner";
 
-storiesOf("Row Animation", module).add("Appear from the top", () => (
-  <TopToBottomRowAnimation />
-));
+storiesOf("Row Animation", module).
+  add("Appear from the left", () => (
+    <LeftToRightRowAnimation />
+  ))
+  .add("Appear from the top", () => (
+    <TopToBottomRowAnimation />
+  ));
 
 const dimensions = Dimensions.get("window");
 
 storiesOf("Login Screen", module).add("Login Screen", () => <LoginScreen />);
-// storiesOf("Simple animations", module)
-//   .add("Shake text field", () => <ShakyText error="Test" />)
-//   .add("Animated bar", () => (
-//     <View style={{ flex: 1, justifyContent: "center" }}>
-//       {seq(7, () => (
-//         <AnimatedBar
-//           key={`${Math.random() * 1000}`}
-//           value={Math.random() * dimensions.width}
-//           delay={500}
-//         />
-//       ))}
-//     </View>
-//   ))
-//   .add("Grow and disappear", () => <GrowAndDisappear />)
-//   .add("Banner", () => (
-//     <Banner>
-//       <Text
-//         style={{
-//           color: "white",
-//           fontWeight: "bold",
-//           textAlign: "center",
-//           justifyContent: "center",
-//           padding: 0,
-//           margin: 0
-//         }}
-//       >
-//         Some text
-//       </Text>
-//     </Banner>
-//   ));
+storiesOf("Simple animations", module)
+  .add("Shake text field", () => <ShakyText error="Test" />)
+  .add("Animated bar", () => (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      {seq(7, () => (
+        <AnimatedBar
+          key={`${Math.random() * 1000}`}
+          value={Math.random() * dimensions.width}
+          delay={500}
+        />
+      ))}
+    </View>
+  ))
+  .add("Grow and disappear", () => <GrowAndDisappear />)
+  .add("Banner", () => (
+    <Banner>
+      <Text
+        style={{
+          color: "white",
+          fontWeight: "bold",
+          textAlign: "center",
+          justifyContent: "center",
+          padding: 0,
+          margin: 0
+        }}
+      >
+        Some text
+      </Text>
+    </Banner>
+  ));
